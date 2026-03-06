222e729df58b20b202013c4dfadd799891c91ba3
Maria Fernanda Blanco <mafeblanco5@gmail.com>
2026-03-06 01:08:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2189044699-aa75c",
+  "appId": "1:1083438916910:web:398e949189db20e5f386bf",
+  "apiKey": "AIzaSyDFma1u3MR9HcIUse13Trgwi_bsphB2qoU",
+  "authDomain": "studio-2189044699-aa75c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1083438916910"
+};
